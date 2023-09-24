181d23edd159c25c1d0ed615bd8c4e87f814fef2
Kashaf-Ansari-3108 <kashafansari3108@gmail.com>
2023-09-24 19:18:33 +0500

    JS Questions 1-60

---
+++ JS QUESTIONS 1-60/chapter1.js
@@ -0,0 +1,12 @@
+//Question 1
+alert("Kashaf");
+alert("Ansari");
+alert("kashafansari3108@gmail.com");
+alert("03490219961");
+alert("f00tb@ll");
+
+//Question 2
+alert("You're learning JavaScript!");
+
+//Question 3
+alert("My Name is Kashaf Ansari");
---
+++ JS QUESTIONS 1-60/chapter2.js
@@ -0,0 +1,14 @@
+//q1
+let myVar;
+
+//q2
+let myName;
+myName="Kashaf";
+
+//q3
+const teamName="Badgers";
+alert(teamName);
+
+//q4
+var bestMan = "Charlie";
+bestMan="John";
